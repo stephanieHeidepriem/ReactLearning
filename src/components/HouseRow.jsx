20f62d5c605aeca8510846619f236c644255dd02
Stephanie Heidepriem <stephanie.heidepriem@gmail.com>
2025-11-02 11:13:11 +0100

    easy formatting HousePrice

--- src/components/HouseRow.jsx
+++ src/components/HouseRow.jsx
@@ -2,11 +2,19 @@ import React from "react";
 import currencyFormatter from "../helpers/currencyFormatter";
 
 const HouseRow = ({house}) => {
+    let priceTd;
+    if (house.price < 500000)
+        priceTd = <td>{currencyFormatter.format(house.price)}</td>;
+    else
+        priceTd = (
+            <td className="text-primary">{currencyFormatter.format(house.price)}</td>
+        );
+
     return (
         <tr>
             <td>{house.address}</td>
             <td>{house.country}</td>
-            <td>{currencyFormatter.format(house.price)}</td>
+            {priceTd}
         </tr>
     );
 }
